d876d7f7420b78bd341a981781c411b7e236de40
燕雨 <35769863+beyondyanyu@users.noreply.github.com>
2019-02-13 21:20:46 +0800

    Create EL取出域中的数据---缺少User.java

---
+++ EL取出域中的数据---缺少User.java
@@ -0,0 +1,68 @@
+<%@page import="java.util.ArrayList"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="beyond.domain.*" %>
+<%@ page import="java.util.*" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+	<!-- 模拟域中的数据 -->
+	<%
+		//存储一个字符串
+		request.setAttribute("company","beyond谚语" );
+		
+		//存储一个对象
+		User user = new User();
+		user.setId(1);
+		user.setName("siqi");
+		user.setPassword("wsq");
+		session.setAttribute("user", user);
+		
+		//存储一个集合
+		List<User> list = new ArrayList<User>();
+		User user1 = new User();
+		//list集合中的第一个元素
+		user1.setId(2);
+		user1.setName("qibao");
+		user1.setPassword("wsq");
+		list.add(user1);
+		
+		//list集合中的第二个元素
+		User user2 = new User();
+		user2.setId(3);
+		user2.setName("yanyu");
+		user2.setPassword("wsq");
+		list.add(user2);
+		
+		
+		application.setAttribute("list", list);
+		
+	%>
+	
+	
+	<!-- 脚本（jsp）的方式取出request域中的值（beyond谚语）  -->
+	<%=request.getAttribute("company") %>
+	
+	<!-- 脚本（jsp）的方式取出User对象中的Name（siqi）中的值  -->
+	<%
+		User sessionUser = (User) session.getAttribute("user");
+		out.write(sessionUser.getName());
+	%>
+	
+	<hr/>
+	
+	
+	<!-- 使用EL表达式获得request域中的值（beyond谚语）  -->
+	${requestScope.company}
+	
+	<!-- 使用EL表达式获得User对象中的Name中的值（siqi） -->
+	${sessionScope.user.name}
+	
+	<!-- 使用EL表达式获得 application域中的第二个元素的name（yanyu） -->
+	${applicationScope.list[1].name}
+</body>
+</html>
